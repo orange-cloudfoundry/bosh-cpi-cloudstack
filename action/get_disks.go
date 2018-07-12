acaccf8cb60a4d6a94f93bec78cb28ed84dd83e0
ArthurHlt <arthur.halet@orange.com>
2018-07-12 13:43:49 +0200

    implements get_disks

--- action/get_disks.go
+++ action/get_disks.go
@@ -2,9 +2,31 @@ package action
 
 import (
 	"github.com/cppforlife/bosh-cpi-go/apiv1"
+	bosherr "github.com/cloudfoundry/bosh-utils/errors"
+	"github.com/orange-cloudfoundry/bosh-cpi-cloudstack/config"
+	"strings"
 )
 
 func (a CPI) GetDisks(cid apiv1.VMCID) ([]apiv1.DiskCID, error) {
-	// todo implement
-	return nil, nil
+	diskCids := make([]apiv1.DiskCID, 0)
+	id, err := a.findVmId(cid)
+	if err != nil {
+		return diskCids, bosherr.WrapErrorf(err, "Cannot getting disks for vm %s", cid.AsString())
+	}
+
+	p := a.client.Volume.NewListVolumesParams()
+	p.SetVirtualmachineid(id)
+	p.SetType(string(config.Datadisk))
+	resp, err := a.client.Volume.ListVolumes(p)
+	if err != nil {
+		return diskCids, bosherr.WrapErrorf(err, "Cannot getting disks for vm %s", cid.AsString())
+	}
+
+	for _, vol := range resp.Volumes {
+		if !strings.HasPrefix(vol.Name, config.PersistenceDiskPrefix) {
+			continue
+		}
+		diskCids = append(diskCids, apiv1.NewDiskCID(vol.Name))
+	}
+	return diskCids, nil
 }
