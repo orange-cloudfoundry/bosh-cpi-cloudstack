89b45e9b50328df4f978a57c96d9339bdf3a0a3b
ArthurHlt <arthur.halet@orange.com>
2018-07-12 16:26:41 +0200

    refactor disk creation

--- action/create_disk.go
+++ action/create_disk.go
@@ -6,11 +6,10 @@ import (
 	"fmt"
 	"github.com/orange-cloudfoundry/bosh-cpi-cloudstack/config"
 	"github.com/satori/go.uuid"
+	"github.com/xanzy/go-cloudstack/cloudstack"
 )
 
 func (a CPI) CreateDisk(size int, props apiv1.DiskCloudProps, cid *apiv1.VMCID) (apiv1.DiskCID, error) {
-	a.client.AsyncTimeout(a.config.CloudStack.Timeout.CreateVolume)
-
 	var diskProps DiskCloudProperties
 	err := props.As(&diskProps)
 	if err != nil {
@@ -23,21 +22,32 @@ func (a CPI) CreateDisk(size int, props apiv1.DiskCloudProps, cid *apiv1.VMCID) 
 		a.logger.Info("create_disk", "Using default disk offering %s because not set in properties", diskOfferName)
 	}
 
+	diskName := fmt.Sprintf("%s%s", config.PersistenceDiskPrefix, uuid.NewV4().String())
+
+	_, err = a.createVolume(diskName, size, diskOfferName, cid)
+	if err != nil {
+		return apiv1.DiskCID{}, bosherr.WrapErrorf(err, "Cannot create disk for vm %s", cid.AsString())
+	}
+
+	return apiv1.NewDiskCID(diskName), nil
+}
+
+func (a CPI) createVolume(diskName string, size int, diskOfferName string, cid *apiv1.VMCID) (*cloudstack.CreateVolumeResponse, error) {
+	a.client.AsyncTimeout(a.config.CloudStack.Timeout.CreateVolume)
+
 	if diskOfferName == "" {
 		diskOfferName = a.config.CloudStack.DefaultOffers.CustomDisk
 		a.logger.Info("create_disk", "Using default custom disk offering %s because there is no default disk offers", diskOfferName)
 	}
 
-	diskName := fmt.Sprintf("%s%s", config.PersistenceDiskPrefix, uuid.NewV4().String())
-
 	zoneId, err := a.findZoneId()
 	if err != nil {
-		return apiv1.DiskCID{}, bosherr.WrapErrorf(err, "Cannot create disk for vm %s", cid.AsString())
+		return nil, bosherr.WrapErrorf(err, "Cannot create disk for vm %s", cid.AsString())
 	}
 
 	offer, err := a.findDiskOfferingByName(diskOfferName)
 	if err != nil {
-		return apiv1.DiskCID{}, bosherr.WrapErrorf(err, "Cannot create disk for vm %s", cid.AsString())
+		return nil, bosherr.WrapErrorf(err, "Cannot create disk for vm %s", cid.AsString())
 	}
 
 	p := a.client.Volume.NewCreateVolumeParams()
@@ -50,10 +60,10 @@ func (a CPI) CreateDisk(size int, props apiv1.DiskCloudProps, cid *apiv1.VMCID) 
 		p.SetSize(int64(size))
 	}
 
-	_, err = a.client.Volume.CreateVolume(p)
+	resp, err := a.client.Volume.CreateVolume(p)
 	if err != nil {
-		return apiv1.DiskCID{}, bosherr.WrapErrorf(err, "Cannot create disk for vm %s", cid.AsString())
+		return nil, bosherr.WrapErrorf(err, "Cannot create disk for vm %s", cid.AsString())
 	}
 
-	return apiv1.NewDiskCID(diskName), nil
+	return resp, nil
 }
